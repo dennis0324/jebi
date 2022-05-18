011882031240076bdd218171073801537f30c9f3
dennis0324 <dennis0324@naver.com>
2022-05-18 09:20:56 +0900

    책 수정추가 컴포넌트 생성

---
+++ src/main/java/io/github/dennis0324/jebi/gui/controller/BookEditAddCompoController.java
@@ -0,0 +1,48 @@
+/*
+ * jebi: A book management software made with JavaFX.
+ * 
+ * Copyright (c) 2022 Dennis Ko (https://github.com/dennis0324)
+ * Copyright (c) 2022 Jaedeok Kim (https://github.com/jdeokkim)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.github.dennis0324.jebi.gui.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class BookEditAddCompoController extends Controller {
+
+
+    @FXML
+    void onBackBtnClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onChangeEditMode(ActionEvent event) {
+
+    }
+    
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void onPageLoad() {
+    }
+
+}
