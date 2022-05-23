beede8ce80e60240556a1f65e086386934a1a0fd
dennis0324 <dennis0324@naver.com>
2022-05-24 00:16:21 +0900

    주석 추가

--- src/main/java/io/github/dennis0324/jebi/gui/event/CustomEventHandler.java
+++ src/main/java/io/github/dennis0324/jebi/gui/event/CustomEventHandler.java
@@ -1,9 +1,34 @@
+/*
+ * jebi: A book management software made with JavaFX.
+ * 
+ * Copyright (c) 2022 Dennis Ko (https://github.com/dennis0324)
+ * Copyright (c) 2022 Jaedeok Kim (https://github.com/jdeokkim)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.dennis0324.jebi.gui.event;
 
 import io.github.dennis0324.jebi.gui.PageLoader;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
-
+/**
+ * 버튼 이벤트 헨들러 
+ * 
+ * @author denis ko
+ * 
+ */
 public class CustomEventHandler implements EventHandler<MouseEvent> {
     private Runnable myMethod;
     private PageLoader pageloader;
